96f93d80043f713ed7709d196f52487f826eeb1e
Björn Pritzel <bjoern.pritzel@aiticon.com>
2017-08-17 11:05:58 +0200

    PYAPP-8 Minor fixes and optimization for the project

--- appngizer/__init__.py
+++ appngizer/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution(__name__).version
+try:
+  __version__ = pkg_resources.get_distribution(__name__).version
+except:
+  __version__ = '1.14.1'
+
 __date__ = '2017-08-11'
 __updated__ = '2017-08-15'
